c7d25ba25705ceee45ec778ae51f280a5d28f79f
bmcinnis-cs <benjamin.mcinnis@crowdstrike.com>
2024-12-12 17:14:27 -0500

    Update provider.tf

--- iac/tmm-gcp/provider.tf
+++ iac/tmm-gcp/provider.tf
@@ -1,11 +1,18 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.13.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5.1"  # You can adjust this version as needed
+    }
+  }
+}
+
 provider "google" {
   project = "tmm-fcs-444213"
 }
 
-terraform {
-	required_providers {
-		google = {
-	    version = "~> 6.13.0"
-		}
-  }
-}
+provider "random" {}
