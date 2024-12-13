344c3a8eabac42b581712361d392359e0003feef
bmcinnis-cs <benjamin.mcinnis@crowdstrike.com>
2024-12-13 11:21:12 -0500

    Update container_cluster.tf

--- iac/tmm-gcp/container_cluster.tf
+++ iac/tmm-gcp/container_cluster.tf
@@ -1,78 +1,29 @@
-# Data source to get the latest valid GKE version
 data "google_container_engine_versions" "gke_version" {
   location       = "us-central1-c"
   version_prefix = "1.31."
 }
 
-resource "google_container_cluster" "gke_cluster" {
-  name               = "boa"
-  location           = "us-central1-c"
-  description        = "cluster for tmm-fcs"
-  project            = "tmm-fcs-444213"
-  networking_mode    = "VPC_NATIVE"
-  network            = "projects/tmm-fcs-444213/global/networks/default"
-  subnetwork         = "projects/tmm-fcs-444213/regions/us-central1/subnetworks/default"
+resource "google_container_cluster" "boa" {
+  name     = "boa"
+  location = "us-central1-c"
+  project  = var.project_id
+
+  # Use the latest valid version from the "REGULAR" channel
+  min_master_version = data.google_container_engine_versions.gke_version.latest_master_version
 
   # Remove default node pool and create custom one
   remove_default_node_pool = true
   initial_node_count       = 1
 
-  # Use the latest valid version from the "REGULAR" channel
-  min_master_version = data.google_container_engine_versions.gke_version.latest_master_version
-
-  # Binary authorization (optional, remove if not needed)
-  binary_authorization {
-    evaluation_mode = "DISABLED"
-  }
+  # Networking (minimal required settings)
+  networking_mode = "VPC_NATIVE"
+  network         = "default"
+  subnetwork      = "default"
 
   # IP allocation policy (required for VPC-native clusters)
-  ip_allocation_policy {
-    cluster_ipv4_cidr_block  = "/14"
-    services_ipv4_cidr_block = "/20"
-  }
-
-  # Private cluster configuration
-  private_cluster_config {
-    enable_private_endpoint = false
-    enable_private_nodes    = false
-    master_ipv4_cidr_block  = "172.16.0.0/28"  # Required even if private nodes are disabled
-  }
-
-  # Release channel
-  release_channel {
-    channel = "REGULAR"
-  }
-
-  # Logging configuration
-  logging_config {
-    enable_components = ["SYSTEM_COMPONENTS", "WORKLOADS"]
-  }
-
-  # Monitoring configuration
-  monitoring_config {
-    enable_components = ["SYSTEM_COMPONENTS", "WORKLOADS"]
-    managed_prometheus {
-      enabled = true
-    }
-  }
-
-  # Addons configuration
-  addons_config {
-    http_load_balancing {
-      disabled = false
-    }
-    horizontal_pod_autoscaling {
-      disabled = false
-    }
-  }
-
-  # Workload Identity configuration
-  workload_identity_config {
-    workload_pool = "tmm-fcs-444213.svc.id.goog"
-  }
+  ip_allocation_policy {}
 }
 
-# Create a custom node pool
 resource "google_container_node_pool" "primary_nodes" {
   name       = "primary-pool"
   location   = "us-central1-c"
@@ -80,27 +31,19 @@ resource "google_container_node_pool" "primary_nodes" {
   node_count = 1
 
   # Use the same version as the master
-  version    = data.google_container_engine_versions.gke_version.latest_master_version
+  version = data.google_container_engine_versions.gke_version.latest_master_version
 
   node_config {
-    preemptible  = false
     machine_type = "e2-medium"
 
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
     service_account = "default"
-    oauth_scopes    = [
-      "https://www.googleapis.com/auth/cloud-platform"
-    ]
-
-    # Enable Workload Identity on the node pool
-    workload_metadata_config {
-      mode = "GKE_METADATA"
-    }
+    oauth_scopes    = ["https://www.googleapis.com/auth/cloud-platform"]
   }
+}
 
-  # Upgrade settings (optional, but recommended)
-  upgrade_settings {
-    max_surge       = 1
-    max_unavailable = 0
-  }
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+  default     = "tmm-fcs-444213"
 }
